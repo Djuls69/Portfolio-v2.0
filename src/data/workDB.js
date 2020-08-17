6c5b7b8e5b2afbc4b80a88a9910afbd39a6400df
Falk <julien.delusseau@gmail.com>
2020-08-17 13:41:10 +0200

    First commit

---
+++ src/data/workDB.js
@@ -0,0 +1,42 @@
+const workDB = [
+  {
+    tag: 'HTML, SASS, Javascript, React, Redux, Firebase',
+    titre: 'CRWN Clothing',
+    img: require('../assets/project05.png'),
+    desc:
+      "Développement d'une e-commerce, à l'aide de React et Redux, utilisant Firebase pour la base de données et l'authentification, intégrant multiples librairies, tels que Stripe pour le paiement en ligne, ou Reselect pour la performance.",
+    link: 'https://react-crwnclothing-app.herokuapp.com'
+  },
+  {
+    tag: 'HTML, CSS, Javascript, React, Material UI',
+    titre: 'Trafalgar',
+    img: require('../assets/project04.png'),
+    desc:
+      "Développement d'une landing page, exclusivement en React, à partir d'une maquette réalisé par un Web Designer professionnel.",
+    link: 'https://hungry-goodall-0f9d3e.netlify.app'
+  },
+  {
+    tag: 'HTML, CSS, Javascript, React',
+    titre: 'Youtube Clone',
+    img: require('../assets/project01.png'),
+    desc: "Un Youtube like, en travaillant avec l'API de Youtube, et réalisé avec React.",
+    link: 'https://sad-darwin-15f574.netlify.app'
+  },
+  {
+    tag: 'HTML, CSS, Javascript, React',
+    titre: 'Nexter',
+    img: require('../assets/project02.png'),
+    desc: "Intégration d'un site fictif pour agence immobilière, à partir d'une maquette réalisé par un web designer.",
+    link: 'https://gracious-hodgkin-bb208b.netlify.app'
+  },
+  {
+    tag: 'HTML, CSS, Javascript, React',
+    titre: 'ComplexApp',
+    img: require('../assets/project03.png'),
+    desc:
+      "Réalisation d'un projet de réseau social, en se connectant à un back-end déjà existant. Vous pouvez créer un compte, créer des posts, rechercher des personnes préalablement enregistrées, etc...",
+    link: 'https://practical-hodgkin-fa6bc0.netlify.app'
+  }
+]
+
+export default workDB
